08b806ba55c3a178037fc29b6448d4ec3eb566e4
djedd1ne <mekki.djallaleddine@gmail.com>
2023-08-01 23:50:25 +0200

    fix format, add brackets

--- ex00/Animal.cpp
+++ ex00/Animal.cpp
@@ -13,25 +13,25 @@
 #include "Animal.hpp"
 
 Animal::Animal() : _type("Animal") {
-    std::cout << G << "Animal default constructor" << C_OFF << std::endl;
+    std::cout << G << "[Animal] default constructor" << C_OFF << std::endl;
 }
 Animal::Animal(const std::string &type) : _type(type) {
-    std::cout << G << "Animal Type constructor" << C_OFF << std::endl;
+    std::cout << G << "[Animal] Type constructor" << C_OFF << std::endl;
 }
 Animal::Animal(const Animal &copy) {
-    std::cout << G << "Animal copy constructor" << C_OFF << std::endl;
+    std::cout << G << "[Animal] copy constructor" << C_OFF << std::endl;
 }
 Animal& Animal::operator=(const Animal &obj) {
     this->_type = obj._type;
     return (*this);
 }
 Animal::~Animal() {
-    std::cout << G << "Animal destructor" << C_OFF << std::endl;
+    std::cout << G << "[Animal] destructor" << C_OFF << std::endl;
 }
         
 std::string Animal::getType(void) const {
     return (this->_type);
 }
 void Animal::makeSound() const {
-    std::cout << G <<  "* UNDEFINED SOUND*" << C_OFF << std::endl;
+    std::cout << G <<  "* UNDEFINED SOUND *" << C_OFF << std::endl;
 }
